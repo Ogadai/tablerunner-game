646869a184fa51f086cf59061c9a2f9c60df9464
Ogadai <andy.lee.surfer@gmail.com>
2026-08-20 16:19:16 +0100

    Added unit tests config

---
+++ app/page.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+describe("Page", () => {
+  it("renders a heading", () => {
+    render(<Page />);
+
+    expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument();
+  });
+});
