7cad9a689ed0ae8e12113a7595f3b566ee0cf13b
Ogadai <andy.lee.surfer@gmail.com>
2026-08-20 16:47:24 +0100

    Added simple gameId check

--- app/[boardId]/[gameId]/page.tsx
+++ app/[boardId]/[gameId]/page.tsx
@@ -1,9 +1,15 @@
 import styles from "./page.module.css";
+import { gameList } from "../../../games/games";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ boardId: string, gameId: string }> }) {
   const params = await props.params;
   const boardId = params.boardId;
   const gameId = params.gameId;
+
+  if (!(gameId in gameList)) {
+    notFound();
+  }
 
   return (
     <div>
---
+++ games/games.tsx
@@ -0,0 +1,3 @@
+export const gameList: { [key: string]: {} } = {
+  test: {}
+};
