8d354aa038fefecd496520740e8a7f913ea44376
Ogadai <andy.lee.surfer@gmail.com>
2026-09-11 07:38:41 +0100

    fixed bug

--- lib/store/gameState.ts
+++ lib/store/gameState.ts
@@ -138,12 +138,9 @@ export async function createPlayerForGame(boardId: string, mapId: string, player
 
     const equipment = characterDef.equipment.map(e => createItemForInventory(gameState, e));
 
-    const name = pickCharacterName(characterDef.id);
-
     const newPlayer: PlayerState = {
       id: playerId,
-      name,
-      originalName: name,
+      name: pickCharacterName(characterDef.id),
       rgbColour: characterDef.rgbColour,
       location: gameDef.locations.find(l => l.id === gameDef.startLocation)!,
       characterStats: { ...characterDef.characterStats },
