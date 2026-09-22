139b0cac825d6962986b0f18d53e2fdc1310ade5
Ogadai <andy.lee.surfer@gmail.com>
2026-09-22 11:48:52 +0100

    Lich King video

--- lib/messages/video-list.ts
+++ lib/messages/video-list.ts
@@ -1,3 +1,4 @@
 export const videoList: { [id: string]: string } = {
   'dragon-wakes': 'https://eivregtw0c7oknqs.public.blob.vercel-storage.com/dragon-wakes.mp4',
+  'lich-king-stand': 'https://eivregtw0c7oknqs.public.blob.vercel-storage.com/lich-king-stand.mp4',
 };
